0c7d3fdcd223a62b4473fe628e69d1758e52fea7
Muhammed Zain <muhammedzain2256@gmail.com>
2024-01-25 21:13:52 +0500

    SignOut Functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -64,4 +64,13 @@ export const deleteUser = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+};
+
+export const signout = (req, res) => {
+  try {
+    res.clearCookie('access_token').status(200).json("User has been signed out");
+  } 
+  catch (error) {
+    next(error);  
+  }
 }
