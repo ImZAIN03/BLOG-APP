a5df131b46b49525499c1a9c8e095b1257e37ac2
Muhammed Zain <muhammedzain2256@gmail.com>
2024-02-06 21:35:44 +0500

    About Page and Project Page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,37 @@
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-4xl font font-semibold text-center my-7'>
+            About Zain' Blog
+          </h1>
+          <div className='text-lg text-gray-800 dark:text-slate-100 font-semibold  flex flex-col gap-6'>
+            <p>
+              Welcome to Zain's Blog! This blog was created by Zain and his team
+              as a personal project to share his thoughts and ideas with the
+              world.
+            </p>
+            <p>
+              Zain is a Web Developer who has two plus years of experience in Full-Stack Web Development, WordPress Development, Shopify Development and also has done projects of Graphics Designing and Blog Writing. 
+            </p>
+
+            <p>
+              On this blog, you'll find weekly articles and tutorials on topics
+              such as web development, software engineering, and programming
+              languages. Zain is always learning and exploring new
+              technologies, so be sure to check back often for new content!
+            </p>
+
+            <p>
+              We encourage you to leave comments on our posts and engage with
+              other readers. You can like other people's comments and reply to
+              them as well. We believe that a community of learners can help
+              each other grow and improve.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,5 +1,11 @@
+import CallToAction from '../components/CallToAction';
+
 export default function Projects() {
   return (
-    <div>Projects</div>
+    <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
+      <h1 className='text-4xl font-semibold'>Projects</h1>
+      <p className='text-md font-semibold text-gray-500 dark:text-gray-100'>Build fun and engaging projects while learning HTML, CSS, and JavaScript!</p>
+      <CallToAction />
+    </div>
   )
 }
